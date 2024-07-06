8cc70af1bf8bde7d7a9343db060116f6cc34b6dd
yaramiu <yaramiu@gmail.com>
2024-07-05 20:02:40 -0400

    Add insert and deleteItem methods

--- tree.js
+++ tree.js
@@ -28,6 +28,61 @@ class Tree {
 
     return root;
   }
+
+  insert(value) {
+    function insertToLeafNode(currentNode, nodeToInsert) {
+      if (currentNode === null) {
+        return nodeToInsert;
+      }
+
+      if (nodeToInsert.data < currentNode.data) {
+        currentNode.left = insertToLeafNode(currentNode.left, nodeToInsert);
+      } else {
+        currentNode.right = insertToLeafNode(currentNode.right, nodeToInsert);
+      }
+
+      return currentNode;
+    }
+
+    const newNode = new Node(value);
+    this.root = insertToLeafNode(this.root, newNode);
+  }
+
+  deleteItem(value) {
+    function findReplacement(currentNode) {
+      if (currentNode.left) {
+        currentNode = findReplacement(currentNode.left);
+      }
+      return currentNode;
+    }
+
+    function deleteNode(currentNode, nodeToDelete) {
+      if (currentNode === null) {
+        return currentNode;
+      }
+
+      if (nodeToDelete.data < currentNode.data) {
+        currentNode.left = deleteNode(currentNode.left, nodeToDelete);
+      } else if (nodeToDelete.data > currentNode.data) {
+        currentNode.right = deleteNode(currentNode.right, nodeToDelete);
+      } else {
+        if (!currentNode.left) {
+          return currentNode.right;
+        } else if (!currentNode.right) {
+          return currentNode.left;
+        }
+
+        const replacementNode = findReplacement(currentNode.right);
+        deleteNode(currentNode, replacementNode);
+        currentNode.data = replacementNode.data;
+      }
+
+      return currentNode;
+    }
+
+    const nodeToDelete = new Node(value);
+    this.root = deleteNode(this.root, nodeToDelete);
+  }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
